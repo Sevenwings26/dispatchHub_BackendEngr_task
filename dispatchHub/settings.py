7a9ea5f4c0acf832615c749baa2745ac5f36fd67
Iyanu Arowosola <iarowosola@yahoo.com>
2025-10-20 14:20:42 +0100

    Ready for submission

--- dispatchHub/settings.py
+++ dispatchHub/settings.py
@@ -96,11 +96,11 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = "dispatchHub.urls"
 
-CORS_ALLOWED_ORIGINS = [
-    "http://127.0.0.1:8000",
-    "http://localhost:8000",
-    os.getenv("RENDER_HOST_URL"),
-]
+# CORS_ALLOWED_ORIGINS = [
+#     "http://127.0.0.1:8000",
+#     "http://localhost:8000",
+#     os.getenv("RENDER_HOST_URL"),
+# ]
 
 TEMPLATES = [
     {
